42bf41f58d76d00ed1f59029f898e37b8de31e8b
Tejaswini BK <tejaswinibk0910@gmail.com>
2026-04-03 15:48:46 -0400

    Add UofR favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: 'Data Services Annual Report 2024-2025',
   description: 'University of Rochester Libraries - Data Services Team Annual Report',
   robots: 'noindex, nofollow',
+  icons: {
+    icon: '/uor_logo.webp',
+  },
 }
 
 export default function RootLayout({
